7a1a23ca050fad76f947e16b1c7f797913ec0c72
AM <armen.melkonyan.93@inbox.ru>
2025-12-09 13:18:10 +0300

    added ci-cd settings

---
+++ __tests__/server.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../server');
+
+it('responds with Hello, World!', async () => {
+  const response = await request(app).get('/');
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('<h1>Hello World!!!</h1>');
+})
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!!!</h1>");
+  res.status(200).send("<h1>Hello World!!!</h1>");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
